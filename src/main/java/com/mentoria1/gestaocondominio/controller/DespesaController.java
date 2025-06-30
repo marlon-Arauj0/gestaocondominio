29c9c80881a0134d60fa9d82690fb27518adeb5d
marlon-Arauj0 <marlonaraujo1303@gmail.com>
2025-06-30 20:54:56 -0300

    #3

--- src/main/java/com/mentoria1/gestaocondominio/controller/DespesaController.java
+++ src/main/java/com/mentoria1/gestaocondominio/controller/DespesaController.java
@@ -1,14 +1,19 @@
 package com.mentoria1.gestaocondominio.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mentoria1.gestaocondominio.dataTransferObjectDTO.DespesaRequest;
 import com.mentoria1.gestaocondominio.service.DespesaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("api/Despesa")
 public class DespesaController {
 
-    private DespesaService service;
+    private final DespesaService service;
 
     @PostMapping
     public void cadastrarDespesa(@RequestBody DespesaRequest request) {
---
+++ src/main/java/com/mentoria1/gestaocondominio/service/DespesaServiceImpl.java
@@ -0,0 +1,16 @@
+package com.mentoria1.gestaocondominio.service;
+
+import com.mentoria1.gestaocondominio.dataTransferObjectDTO.DespesaRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DespesaServiceImpl implements DespesaService{
+
+    @Override
+    public void cadastrar(DespesaRequest request) {
+        System.out.println("falta implementar");
+
+
+
+    }
+}
