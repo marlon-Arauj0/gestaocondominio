2d3e7076f4926979b45f63b749ad52b130fc03b5
marlon-Arauj0 <marlonaraujo1303@gmail.com>
2025-07-15 21:43:22 -0300

    #11

--- src/main/resources/static/V2__cria_tabela_usuarios.sql
+++ src/main/resources/static/V2__cria_tabela_usuarios.sql
@@ -1,7 +1,9 @@
 CREATE TABLE usuarios (
-    id                  BIGINT               NOT NULL   PRIMARY KEY  AUTO_INCREMENT    ,
-    nome                VARCHAR(255)         NOT NULL                                  ,
-    email               VARCHAR(255)         NOT NULL   UNIQUE                         ,
-    senha               VARCHAR(255)         NOT NULL                                  ,
-    permitir_entrada    BOOLEAN              NOT NULL   DEFAULT FALSE
+    id              BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    id_unidade      BIGINT          NOT NULL                            ,
+    nome            VARCHAR(255)    NOT NULL                            ,
+    email           VARCHAR(255)    NOT NULL UNIQUE                     ,
+    senha           VARCHAR(255)    NOT NULL                            ,
+    admin           BOOLEAN         NOT NULL DEFAULT false              ,
+    CONSTRAINT fk_usuarios_unidades FOREIGN KEY (id_unidade) REFERENCES unidades(id)
 );
---
+++ src/main/resources/static/V4__cria_tabela_receita.sql
@@ -0,0 +1,12 @@
+CREATE TABLE receitas (
+    id              BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    id_unidade      BIGINT          NULL                                ,
+    id_usuario      BIGINT          NOT NULL                            ,
+    categoria       VARCHAR(255)    NOT NULL                            ,
+    descricao       VARCHAR(255)    NOT NULL                            ,
+    valor           DECIMAL(10, 2)  NOT NULL                            ,
+    data_pagamento  DATE            NULL                                ,
+    data_vencimento DATE            NOT NULL                            ,
+    data_cricao     TIMESTAMP       NOT NULL DEFAULT NOW()              ,
+    CONSTRAINT fk_receitas_usuarios FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+);
