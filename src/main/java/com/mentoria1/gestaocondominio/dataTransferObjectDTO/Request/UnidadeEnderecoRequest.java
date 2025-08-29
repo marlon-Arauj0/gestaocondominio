1fb935ecc246586646b2308629de2ca13e58140a
marlon-Arauj0 <marlonaraujo1303@gmail.com>
2025-08-28 22:58:07 -0300

    funcionou

--- src/main/java/com/mentoria1/gestaocondominio/dataTransferObjectDTO/Request/UnidadeEnderecoRequest.java
+++ src/main/java/com/mentoria1/gestaocondominio/dataTransferObjectDTO/Request/UnidadeEnderecoRequest.java
@@ -1,7 +1,5 @@
 package com.mentoria1.gestaocondominio.dataTransferObjectDTO.Request;
 
-import com.mentoria1.gestaocondominio.domain.Unidade;
-
 public record UnidadeEnderecoRequest (Long unidadeId,
                                      String cep,
                                      String numero,
